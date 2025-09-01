d356b5a814b7131cb7d090e01ae9ac10478b7994
Chum Chantha <chumchantha22y@gmail.com>
2025-09-01 16:55:16 +0700

    style(home): change h1

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "@/components/ThemeToggle";
 export default async function Home() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-[40vh]">
-      <h1>Hello Hi</h1>
+      <h1>Hi</h1>
       <ThemeToggle />
     </div>
   );
